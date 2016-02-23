f7d94faf577149da0418307f77334191c38303f7
Markku Rontu <markku.rontu@iki.fi>
2016-02-23 21:17:24 +0200

    refactor: move code out of this project

--- src/raspispy/core.cljs
+++ src/raspispy/core.cljs
@@ -19,12 +19,6 @@
 
 
 
-;; app data
-(def known-beacons
-  {"ec00fb52-740a-40f6-aa96-2d4d8120c567" :hsl})
-
-
-
 ;; iBeacon functions
 
 (defn get-state []
@@ -46,35 +40,19 @@
          (apply concat)
          (apply str))))
 
-(defn distance [tx-power rssi]
-  (Math/pow 10 (* 0.05 (- tx-power rssi))))
-
-(defn accuracy [tx-power rssi]
-  (if (<= (Math/abs rssi) 0.0001)
-    -1
-    (let [ratio (/ rssi tx-power)]
-      (if (< ratio 1.0)
-        (Math/pow ratio 10)
-        (+ (* 0.89976 (Math/pow ratio 7.7095)) 0.111)))))
-
 
 
 ;; event handlers
 
 (defn on-discover [peripheral]
-  (let [advertisement (js->clj (.-advertisement peripheral) :keywordize-keys true)
-        tx-power-level (:txPowerLevel advertisement)
-        service-uuids (:serviceUuids advertisement)
-        manufacturer-data (:manufacturerData advertisement)
+  (let [advertisement (.-advertisement peripheral)
+        manufacturer-data (.-manufacturerData advertisement)
         rssi (.-rssi peripheral)]
     (when (and manufacturer-data
                (ibeacon? manufacturer-data))
       (let [uuid (parse-uuid manufacturer-data)
-            tx-power (.readInt8 manufacturer-data 24)
-            beacon-name (name (get known-beacons uuid :unknown))
-            d (.toFixed (distance tx-power rssi) 2)
-            a (.toFixed (accuracy tx-power rssi) 2)]
-        (println "Discovered uuid" uuid "tx-power" tx-power "rssi" rssi "name" beacon-name d "±" a "m")))))
+            tx-power (.readInt8 manufacturer-data 24)]
+        (println "Discovered uuid" uuid "tx-power" tx-power "rssi" rssi)))))
 
 (defn start-discovery! []
   (println "Starting discovery!")
